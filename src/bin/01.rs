e18bafeb9390a1d34d3359fb3eea08d7cb0a8c13
Calum <calum@calumm.dev>
2024-12-02 09:02:43 +0000

    feat: solve day 1 part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,32 @@
+use itertools::Itertools;
+use std::str::FromStr;
+
 advent_of_code::solution!(1);
 
+// Disclaimer - I am making extensive use of improper practices for production code, for example the
+// liberal use of `.unwrap()`.
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let (mut left, mut right): (Vec<u32>, Vec<u32>) = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .collect_tuple::<(&str, &str)>()
+                .unwrap()
+        })
+        .map(|(left, right)| (u32::from_str(left).unwrap(), u32::from_str(right).unwrap()))
+        .unzip();
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let result = left
+        .iter()
+        .zip(right.iter())
+        .map(|(left, right)| left.abs_diff(*right))
+        .sum::<u32>();
+
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
