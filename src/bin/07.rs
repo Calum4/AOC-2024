f0db9a92ae0a2afdc0cd5f2dd0d3e500ffd65685
Calum <calum@calumm.dev>
2025-03-09 21:30:42 +0000

    feat(day7): solve part 2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -4,7 +4,7 @@ use rayon::prelude::*;
 
 advent_of_code::solution!(7);
 
-fn is_equation_valid((result, values): (u64, Vec<u64>)) -> Option<u64> {
+fn is_equation_valid_pt1((result, values): (u64, Vec<u64>)) -> Option<u64> {
     for mut i in 0..=((u64::MAX << (values.len() - 1)) ^ u64::MAX) {
         let calculated_result = values.iter().copied().reduce(|acc, value| {
             let acc = match i & 1 {
@@ -12,11 +12,11 @@ fn is_equation_valid((result, values): (u64, Vec<u64>)) -> Option<u64> {
                 1 => acc * value,
                 _ => panic!("This should not be possible")
             };
-            
+
             i >>= 1;
             acc
         }).unwrap();
-        
+
         if result == calculated_result {
             return Some(result)
         }
@@ -37,14 +37,71 @@ pub fn part_one(input: &str) -> Option<u64> {
 
             (result, values)
         })
-        .filter_map(is_equation_valid)
+        .filter_map(is_equation_valid_pt1)
         .sum();
 
     Some(sum)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+fn is_equation_valid_pt2((result, values): (u64, Vec<u64>)) -> Option<u64> {
+    let num_ops = values.len() - 1;
+
+    let mut result_vec = vec![0; num_ops];
+
+    for i in 0..3u32.pow(num_ops as u32) {
+        if i > 0 {
+            result_vec.fill(0);
+        }
+
+        let mut value = i;
+
+        for result in result_vec.iter_mut().rev() {
+            if value == 0 {
+                break;
+            }
+
+            *result = (value % 3) as u8;
+            value /= 3;
+        }
+
+        let mut result_vec_iter = result_vec.iter();
+
+        let calculated_result = values.iter().copied().reduce(|acc, value| {
+            match result_vec_iter.next().unwrap() {
+                0 => acc + value,
+                1 => acc * value,
+                2 => {
+                    let concat = format!("{acc}{value}");
+                    concat.parse().unwrap()
+                }
+                _ => panic!("This should not be possible")
+            }
+        }).unwrap();
+
+        if result == calculated_result {
+            return Some(result)
+        }
+    }
+
     None
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let sum = input
+        .par_lines()
+        .map(|line| {
+            let mut split_line = line
+                .split(":");
+
+            let result = split_line.next().map(u64::from_str).unwrap().unwrap();
+            let values = split_line.next().unwrap().split_ascii_whitespace().flat_map(u64::from_str).collect_vec();
+
+            (result, values)
+        })
+        .filter_map(is_equation_valid_pt2)
+        .sum();
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -60,6 +117,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
     }
 }
